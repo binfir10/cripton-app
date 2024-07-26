bcf7ea2aaa72ae825c31d3da59cdc14c7f8ac3f0
Brian Figueroa <bryan09figueroa@gmail.com>
2024-07-26 02:48:06 -0300

    Actualizar adventaje.tsx

--- components/section/adventaje.tsx
+++ components/section/adventaje.tsx
@@ -8,7 +8,7 @@ export default function Adventaje() {
         <h2 className="text-2xl md:text-5xl font-jakarta">
           Automated portfolio tracking
         </h2>
-        <p className="text-secondary text-center md:w-2/3">
+        <p className="text-secondary text-center w-2/3">
           Lorem ipsum dolor sit amet. Qui consequatur sint 33 voluptatem officia
           et sint laboriosam sed ipsa sint ut voluptatum labore et possimus
           voluptas.
